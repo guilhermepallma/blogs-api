884a5f1762b7e7ece008ccbc0d22e0f0759a2c11
Guilherme Palma <guilhermepallma@gmail.com>
2022-10-27 13:54:21 -0400

    ✅ Requisito 01 - cria migrations

---
+++ src/migrations/20221027153241-create-posts-categories.js
@@ -0,0 +1,30 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', { 
+      postId: {
+        type: Sequelize.INTEGER,
+        field: 'post_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        reference: {
+          model: 'blog_posts',
+          key: 'id',
+        }
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        field: 'category_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        reference: {
+          model: 'categories',
+          key: 'id',
+        }
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('posts_categories');
+  }
+};
