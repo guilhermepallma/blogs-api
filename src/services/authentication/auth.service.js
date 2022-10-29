a0751bb969ec78338a74531f12fea05556085134
Guilherme Palma <guilhermepallma@gmail.com>
2022-10-29 01:24:58 -0400

    🛠 Corrigindo erro do service

--- src/services/authentication/auth.service.js
+++ src/services/authentication/auth.service.js
@@ -1,6 +1,6 @@
 const { validateLogin } = require('../schemas/validade.values');
-const { jwt } = require('../../utils/jwt.utils');
 const { User } = require('../../models');
+const { jwt } = require('../../utils');
 
 const validateUser = async ({ email, password }) => {
   const error = await validateLogin({ email, password });
